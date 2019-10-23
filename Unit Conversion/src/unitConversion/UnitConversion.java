33e2f2ec66a78e9e59bbfd52fe32e4980ca20e32
340016 <340016@152-ZMED-352.mcpsmd.org>
2019-10-23 11:58:41 -0400

    Updated program classes

--- Unit Conversion/src/unitConversion/UnitConversion.java
+++ Unit Conversion/src/unitConversion/UnitConversion.java
@@ -6,110 +6,57 @@ public class UnitConversion {
 	private static String unitTo;
 	private static double fromAmount;
 	
-	//code below is wrong, write a class that checks if inputted units are valid, and then a class that compares the units to see if its a valid conversion. then write conversion class
+	public static boolean validUnits(String from, String to) {
+		unitFrom = from;
+		unitTo = to;
+		
+		if (!(unitFrom.matches("fl. oz|gal|oz|lb|in|ft|mi"))) {
+			return false;
+		}
+		else if (!(unitTo.matches("ml|l|g|kg|mm|cm|m|km"))) {
+			return false;
+		}
+		else {
+			validConversion(unitFrom, unitTo);
+			return true;
+		}	
+		
+	}
 	
 	public static boolean validConversion(String from, String to) {
 		unitFrom = from;
 		unitTo = to;
 		
-		if ((unitFrom.equals("fl. oz") || unitFrom.equals("gal")) && (!(unitTo.equals("gal")) || !(unitTo.equals("fl. oz")))) {
-			return false;
-		} 
-		else if ((unitFrom.equals("oz") || unitFrom.equals("lb")) && (!(unitTo.equals("oz")) || !(unitTo.equals("lb")))) {
+		if (unitFrom.matches("fl. oz|gal") && (!(unitTo.matches("ml|l")))) {
 			return false;
 		}
-		else if ((unitFrom.equals("in") || unitFrom.equals("ft") || unitFrom.equals("mi")) && (!(unitTo.equals("in")) || !(unitTo.equals("ft")) || !(unitTo.equals("mi")))) {
+		else if (unitFrom.matches("oz|lb") && (!(unitTo.matches("g|kg")))) {
+			return false;
+		}
+		else if (unitFrom.matches("in|ft|mi") && (!(unitTo.matches("mm|cm|m|km")))) {
 			return false;
 		}
 		else {
+			convert(unitFrom, unitTo, fromAmount); //find a way to set fromAmount
 			return true;
 		}
-		
 	}
 	
-	public static double convert(double amount) {
+	public static double convert(String from, String to, double amount) { //do i need strings here?
+		unitFrom = from;
+		unitTo = to;
 		fromAmount = amount;
 		
-		if (unitFrom.equals("fl. oz")) {
-			if (unitTo.equals("fl. oz")) {
-				return amount;
-			}
-			else {
-				return amount/128; //1 fluid ounce is 1/128 gallons
-			}
-		}
 		
-		if (unitFrom.equals("gal")) {
-			if (unitTo.equals("gal")) {
-				return amount;
-			}
-			else {
-				return amount * 128;
-			}
-		}
 		
-		if (unitFrom.equals("oz")) {
-			if (unitTo.equals("oz")) {
-				return amount;
-			}
-			else {
-				return amount/16; //16 ounces in one pound
-			}
-		}
-		
-		if (unitFrom.equals("lb")) {
-			if (unitTo.equals("lb")) {
-				return amount;
-			}
-			else {
-				return amount * 16;
-			}
-		}
-		
-		if (unitFrom.equals("in")) {
-			if (unitTo.equals("in")) {
-				return amount;
-			}
-			else if (unitTo.equals("ft")) {
-				return amount/12; //12 inches in 1 foot
-			}
-			else {
-				return amount/63360; //63360 inches in 1 mile
-			}
-		}
-		
-		if (unitFrom.equals("ft")) {
-			if (unitTo.equals("ft")) {
-				return amount;
-			}
-			else if (unitTo.equals("in")) {
-				return amount*12;
-			}
-			else {
-				return amount/5280; //5280 feet in 1 mile
-			}
-		}
-		
-		if (unitFrom.equals("mi")) {
-			if (unitTo.equals("mi")) {
-				return amount;
-			}
-			else if (unitTo.equals("in")) {
-				return amount*63360;
-			}
-			else {
-				return amount*5280; 
-			}
-		}
 	}
 }
-
-
-
-
 /* 
-5.4.  Unit conversion. Write a unit conversion program that asks the users from which unit they want to convert (fl. oz, gal, oz, lb, in, ft, mi) and to which unit they want to 
-convert (ml, l, g, kg, mm, cm, m, km). Reject incompatible conversions (such as gal → km). Ask for the value to be converted, then display the result:
+5.4.  Unit conversion. Write a unit conversion program that asks the users from which 
+unit they want to convert (fl. oz, gal, oz, lb, in, ft, mi) and to which unit they want 
+to 
+convert (ml, l, g, kg, mm, cm, m, km). Reject incompatible conversions (such as gal → km).
+Ask for the value to be converted, then display the result:
 
 Convert from? gal
 Convert to? ml
--- Unit Conversion/src/unitConversion/UnitConversionTester.java
+++ Unit Conversion/src/unitConversion/UnitConversionTester.java
@@ -10,15 +10,15 @@ public class UnitConversionTester {
 		String unitFrom = input.nextLine();
 		System.out.println("(ml, l, g, kg, mm, cm, m, km)\nWhat unit do you want to convert to?");
 		String unitTo = input.nextLine();
-		System.out.println("What is the amount of " + unitFrom + "?");
-		double fromAmount = input.nextDouble();
-		double toAmount;
+		//System.out.println("What is the amount of " + unitFrom + "?");
+		//double fromAmount = input.nextDouble();
+		//double toAmount;
 		
-		//if (UnitConversion.validConversion(unitFrom, unitTo)) {
-		//	UnitConversion.convert(fromAmount);
-		//}
 		
-		//System.out.println(fromAmount + " " + unitFrom + " = " + toAmount + " " + unitTo);
+		UnitConversion.validUnits(unitFrom, unitTo); //final method call
+		System.out.println(UnitConversion.validUnits(unitFrom, unitTo)); //tester outputs
+		System.out.println(UnitConversion.validConversion(unitFrom, unitTo));
+		
 	}
 
 }
