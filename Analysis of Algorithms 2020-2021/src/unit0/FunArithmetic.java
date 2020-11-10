f96e70b331a337256a853a03fdabcc3f689394b0
Noah <54943415+Spacetastics@users.noreply.github.com>
2020-11-10 13:41:57 -0500

    Test lab for new grading system

---
+++ Analysis of Algorithms 2020-2021/src/unit0/FunArithmetic.java
@@ -0,0 +1,27 @@
+package unit0;
+
+public class FunArithmetic {
+
+  public static int add(int a, int b) {
+	  //TODO write code that returns a + b
+	  return a + b;
+  }
+
+  public static int subtract(int a, int b) {
+	  //TODO write code that returns a - b
+	  return a - b;
+  }
+
+  public static int multiply(int a, int b) {
+	  // TODO write code that returns a * b
+	  return a * b;
+  }
+
+  public static void main(String[] code) {
+	  int a = 3, b = 2;
+	  System.out.println("a + b = " + add(a, b));
+	  System.out.println("a - b = " + subtract(a, b));
+	  System.out.println("a * b = " + multiply(a, b));
+  }
+  
+}
---
+++ Analysis of Algorithms 2020-2021/src/unit3/WarmUp_Oct8.java
@@ -0,0 +1,18 @@
+package unit3;
+
+import java.util.ArrayList;
+
+public class WarmUp_Oct8 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1); list.add(3); list.add(5); list.add(7); list.add(2); list.add(8); list.add(6);
+		System.out.println(list.toString());
+		list.remove(2);
+		System.out.println(list.toString());
+		
+		//how to make this code work for a linked list
+		
+	}
+	
+}
