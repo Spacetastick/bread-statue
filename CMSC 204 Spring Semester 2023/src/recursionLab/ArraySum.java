2faa94ed555558d314187d9e01460f5bf2f4961f
Noah <54943415+Spacetastics@users.noreply.github.com>
2023-02-28 22:36:19 -0500

    recursion lab

---
+++ CMSC 204 Spring Semester 2023/src/recursionLab/ArraySum.java
@@ -0,0 +1,13 @@
+package recursionLab;
+
+public class ArraySum {
+	
+	public int sumOfArray(Integer[] a, int index) {
+		int sum = a[index];
+		if (index > 0)
+			sum += sumOfArray(a, index-1);
+		return sum;
+		
+	}
+	
+}
