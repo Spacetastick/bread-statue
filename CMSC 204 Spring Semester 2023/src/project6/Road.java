134e3fa27b6c0461da60bd81f56e18a76bbbfdf0
Noah <54943415+Spacetastics@users.noreply.github.com>
2023-05-05 11:16:57 -0400

    added basic structure for road and town classes

--- CMSC 204 Spring Semester 2023/src/project6/Road.java
+++ CMSC 204 Spring Semester 2023/src/project6/Road.java
@@ -7,4 +7,48 @@ package project6;
  */
 public class Road implements Comparable<Road> {
 
+	public Road(Town source, Town destination, int degrees, String name) {
+		//constructor
+	}
+	
+	public Road(Town source, Town destination, String name) {
+		//constructor with weight preset at 1
+	}
+	
+	public boolean contains(Town town) {
+		//returns true only if the edge is connected to the given vertex
+	}
+	
+	@Override
+	public String toString() {
+		
+	}
+	
+	public String getName() {
+		//returns the road name
+	}
+	
+	public Town getDestination() {
+		//returns the second town on the road
+	}
+	
+	public Town getSource() {
+		//returns the first town on the road
+	}
+	
+	@Override
+	public int compareTo(Road o) {
+		//returns 0 if the road names are the same, a positive or negative number if the road names are not the same
+	}
+	
+	public int getWeight() {
+		//returns the distance of the road
+	}
+	
+	@Override
+	public boolean equals(Object r) {
+		//returns true if each of the ends of the road r is the same as the ends of this road. 
+		//Remember that a road that goes from point A to B is the same as a road that goes from point B to A
+	}
+	
 }
--- CMSC 204 Spring Semester 2023/src/project6/Town.java
+++ CMSC 204 Spring Semester 2023/src/project6/Town.java
@@ -9,4 +9,38 @@ package project6;
  */
 public class Town implements Comparable<Town> {
 
+	public Town(String name) {
+		//constructor
+	}
+	
+	public Town(Town templateTown) {
+		//copy constructor
+	}
+	
+	public String getName() {
+		//return the town's name
+	}
+	
+	@Override
+	public int compareTo(Town o) {
+		//compare to method
+		//returns 0 if names are equal, a positive or negative number if the names are not equal
+	}
+	
+	@Override
+	public String toString() {
+		//to string method
+		//returns the town name
+	}
+	
+	@Override
+	public int hashCode() {
+		//returns the hashcode for the name of the town
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		//returns true if the town names are equal, false if not
+	}
+	
 }
