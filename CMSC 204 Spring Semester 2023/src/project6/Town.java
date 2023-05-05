12f8f22bbdb11adbd10dd3dd0689d00e62ad63c8
Noah <54943415+Spacetastics@users.noreply.github.com>
2023-05-05 12:01:06 -0400

    town instance variables added and methods implemented + 3 getter/setter methods added

--- CMSC 204 Spring Semester 2023/src/project6/Town.java
+++ CMSC 204 Spring Semester 2023/src/project6/Town.java
@@ -1,5 +1,7 @@
 package project6;
 
+import java.util.ArrayList;
+
 /*.
  * Create a Town class that holds the name of the town and a list of adjacent towns, and other fields as desired, and the traditional methods (constructors,
  * getters/setters, toString, etc).
@@ -9,38 +11,77 @@ package project6;
  */
 public class Town implements Comparable<Town> {
 
+	private String name;
+	//may need to be updated to a different list type
+	private ArrayList<Town> adjacentTowns = new ArrayList<>();
+	
 	public Town(String name) {
 		//constructor
+		//I'm not sure if this will work given that the variable is private, same for the other constructor. Come back if issue
+		this.name = name;
 	}
 	
 	public Town(Town templateTown) {
 		//copy constructor
+		//I assume this is the correct implementation, but I may need to come back to this
+		this.name = templateTown.name;
+		//might need or want to copy the adjacentTowns list as well
 	}
 	
 	public String getName() {
 		//return the town's name
+		return this.name;
+	}
+	
+	//not provided in javadoc
+	public  void setName(String name) {
+		//sets name of town
+		this.name = name;
+	}
+	
+	//not provided in javadoc
+	public ArrayList<Town> getAdjacentTowns() {
+		//return the town's adjacencies
+		return this.adjacentTowns;
+	}
+	
+	//not provided in javadoc
+	public void setAdjacentTowns(ArrayList<Town> list) {
+		//sets the town's adjacencies
+		//this is probably not the correct way to implement this 🤷‍
+		this.adjacentTowns = list;
 	}
 	
 	@Override
 	public int compareTo(Town o) {
 		//compare to method
 		//returns 0 if names are equal, a positive or negative number if the names are not equal
+		return this.name.compareTo(o.name);
+		
 	}
 	
 	@Override
 	public String toString() {
 		//to string method
 		//returns the town name
+		return this.name;
 	}
 	
 	@Override
 	public int hashCode() {
 		//returns the hashcode for the name of the town
+		int result = 17;
+		result = 31 * result + name.hashCode();
+		return result;
 	}
 	
 	@Override
 	public boolean equals(Object obj) {
 		//returns true if the town names are equal, false if not
+		if (this.getClass() != obj.getClass())
+			return false;
+		
+		return this.name.equals(((Town)obj).name);
 	}
 	
 }
