62a0847171f414ff224a6b6a941ddce90c80afb9
Noah <54943415+Spacetastics@users.noreply.github.com>
2023-05-10 21:19:57 -0400

    implemented shortestPath and dijkstraShortestPath
    also updated internal structure of graph so that it holds information
    from running dijkstraShortestPath

--- CMSC 204 Spring Semester 2023/src/project6/Graph.java
+++ CMSC 204 Spring Semester 2023/src/project6/Graph.java
@@ -1,6 +1,7 @@
 package project6;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -24,11 +25,13 @@ public class Graph implements GraphInterface<Town, Road> {
 	private HashSet<Town> townSet;
 	private HashSet<Road> roadSet;
 	private HashMap<Town, ArrayList<Road>> adjList;
+	private HashMap<Town, Object[]> shortestPathData;
 	
 	public Graph() {
 		townSet = new HashSet<>();
 		roadSet = new HashSet<>();
 		adjList = new HashMap<>();
+		shortestPathData = new HashMap<>();
 	}
 	
 	public Road getEdge(Town source, Town destination) {
@@ -191,12 +194,105 @@ public class Graph implements GraphInterface<Town, Road> {
 		return this.townSet;
 	}
 	
+	//documentation says nothing about inputs being null or anything, so I assume there will not be any errors tested
 	public ArrayList<String> shortestPath(Town source, Town destination) {
+		ArrayList<String> path = new ArrayList<>();
 		
+		//populates shortestPathData with data for the current state of the graph
+		this.dijkstraShortestPath(source);
+		
+		/*
+		 * Vertex_1 via Edge_2 to Vertex_3 4 (first string in ArrayList)
+		 * Vertex_3 via Edge_5 to Vertex_8 2 (second string in ArrayList)
+		 * Vertex_8 via Edge_9 to Vertex_10 2 (third string in ArrayList)
+		 */
+		
+		//adds strings to path in the above format backwards, going from the destination to the source
+		Town currentVertex = destination;
+		while (currentVertex != source) {
+			Object[] currentData = this.shortestPathData.get(currentVertex);
+			path.add(currentData[1] + " via " + currentData[2] + " to " + currentVertex + " " + ((Road)currentData[2]).getWeight());
+		}
+		
+		//reverses the order of path so strings are in correct order
+		Collections.reverse(path);
+		
+		return path;
 	}
 	
 	//should this be public?
 	public void dijkstraShortestPath(Town source) {
+		//shortestPathData will be used to hold the three values needed per row of the data table for dijkstra.
+		//The current vertex will be held as the key value. The int value for total road weight from the source will be held in index 0
+		//of the size 3 object array, the previous vertex will be held in index 1 of the Object array, and the road between
+		//the current and previous will be held in index 2.
+		//basic overview of value storage for a key: {shortest distance from original source to key, previous vertex from key, road between key and previous vertex}
+		
+		//assumes that source does indeed exist in the graph, I guess I will take care of this in shortestPath
+		ArrayList<Town> visited = new ArrayList<>();
+		ArrayList<Town> unvisited = new ArrayList<>(this.adjList.keySet());
+		
+		//initial setup:
+		//clears the hashmap so that correct data for the current state of the graph is reflected in it
+		this.shortestPathData.clear();
+		
+		//adding all vertices to map with arbitrarily high weight value to be initially compared against
+		for (Town town : unvisited) {
+			Object[] data = {Integer.MAX_VALUE, null, null};
+			this.shortestPathData.put(town, data);
+		}
+		
+		//updating map entry for source vertex to contain 0 weight instead of MAX_VALUE
+		Object[] sourceData = {0, null, null};
+		this.shortestPathData.put(source, sourceData);
+		
+		//start of algorithm:
+		Town currentVertex = source;
+		while (unvisited.size() > 0) {
+			
+			//getting access to all neighbors of current vertex by accessing all connected edges
+			ArrayList<Road> currentNeighbors = this.adjList.get(currentVertex);
+			//looping like this to check all neighboring towns of current vertex
+			for (Road road : currentNeighbors) {
+				
+				//here is where access to the neighboring towns is directly gained
+				Town neighbor;
+				if (road.getSource().equals(currentVertex))
+					neighbor = road.getDestination();
+				else
+					neighbor = road.getSource();
+				
+				//only runs if the current neighbor is unvisited
+				if (unvisited.contains(neighbor)) {
+					//value is shortest distance from starting point stored in currentVertex + the weight of the current road to the neighbor
+					int distanceFromSource = (int)this.shortestPathData.get(currentVertex)[0] + road.getWeight();
+					
+					//if this distance is less than the distance currently stored in the neighbor town as shortest distance from starting point,
+					//then the neighbor's data will be updated to contain the new shortest distance and previous vertex
+					if (distanceFromSource < (int)this.shortestPathData.get(neighbor)[0]) {
+						Object[] newData = {distanceFromSource, currentVertex, road};
+						this.shortestPathData.put(neighbor, newData);
+					}
+				}
+					
+			}
+			
+			//the visit of the current vertex has now been completed, so it will be added to visited and removed from unvisited
+			visited.add(currentVertex);
+			unvisited.remove(currentVertex);
+			
+			//makes current vertex the unvisited Town that is the shortest distance away from the original starting point
+			//only runs when unvisited contains at least one vertex in order to avoid index out of bounds error
+			if (unvisited.size() > 0) {
+				Town nextCurrent = unvisited.get(0);
+				for (Town town : unvisited) {
+					if ((int)this.shortestPathData.get(town)[0] < (int)this.shortestPathData.get(nextCurrent)[0])
+						nextCurrent = town;
+				}
+				currentVertex = nextCurrent;
+			}
+
+		}
 		
 	}
 	
